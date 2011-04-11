89b00e044d50da7f9047cb04ee237c97a3dd7e48
Bulat Shakirzyanov <mallluhuct@gmail.com>
2011-04-11 16:49:29 -0400

    add doc-blocks and 'gigya.unlink' event and action

--- Controller/GigyaController.php
+++ Controller/GigyaController.php
@@ -4,11 +4,25 @@ namespace OpenSky\Bundle\GigyaBundle\Controller;
 
 use OpenSky\Bundle\GigyaBundle\Socializer\Buzz\MessageFactory;
 use OpenSky\Bundle\GigyaBundle\Socializer\Socializer;
-use OpenSky\Component\HttpFoundation\Response;
+use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
 
 class GigyaController
 {
+    /**
+     * @var Symfony\Component\HttpFoundation\Request
+     */
+    private $request;
+
+    /**
+     * @var Symfony\Component\EventDispatcher\EventDispatcherInterface
+     */
+    private $dispatcher;
+
     /**
      * @var OpenSky\Bundle\GigyaBundle\Socializer\Socializer
      */
@@ -30,13 +44,17 @@ class GigyaController
     private $route;
 
     /**
-     * @param OpenSky\Bundle\GigyaBundle\Socializer\Socializer          $socializer
-     * @param OpenSky\Bundle\GigyaBundle\Socializer\Buzz\MessageFactory $messages
-     * @param Symfony\Component\Routing\RouterInterface                 $router
-     * @param string                                                    $route
+     * @param Symfony\Component\HttpFoundation\Request                   $request
+     * @param Symfony\Component\EventDispatcher\EventDispatcherInterface $dispatcher
+     * @param OpenSky\Bundle\GigyaBundle\Socializer\Socializer           $socializer
+     * @param OpenSky\Bundle\GigyaBundle\Socializer\Buzz\MessageFactory  $messages
+     * @param Symfony\Component\Routing\RouterInterface                  $router
+     * @param string                                                     $route
      */
-    public function __construct(Socializer $socializer, MessageFactory $messages, RouterInterface $router, $route)
+    public function __construct(Request $request, EventDispatcherInterface $dispatcher, Socializer $socializer, MessageFactory $messages, RouterInterface $router, $route)
     {
+        $this->request    = $request;
+        $this->dispatcher = $dispatcher;
         $this->socializer = $socializer;
         $this->messages   = $messages;
         $this->router     = $router;
@@ -50,5 +68,22 @@ class GigyaController
         $message = $this->socializer->login($provider);
 
         return new Response($message->getContent(), $message->getStatusCode(), $message->getHeaders());
+    }
+
+    public function unlink($uid)
+    {
+        $provider = $this->request->request->get('provider');
+        $this->socializer->removeConnection($this->socializer->getAccessToken(), $uid, $provider);
+
+        $this->dispatcher->notify(new Event($this, 'gigya.unlinked', array(
+            'provider' => $provider,
+            'uid'      => $uid
+        )));
+
+        if (null !== ($redirect = $this->request->get('redirect'))) {
+            return new RedirectResponse($redirect);
+        }
+
+        return new Response();
     }
 }
