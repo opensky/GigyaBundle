3974e02bb0dc3b7c8e7747e9c6fc7b2a75e4e051
Bulat Shakirzyanov <mallluhuct@gmail.com>
2011-04-15 13:07:39 -0400

    fix redirect route generation

--- Security/Firewall/GigyaListener.php
+++ Security/Firewall/GigyaListener.php
@@ -5,15 +5,49 @@ namespace OpenSky\Bundle\GigyaBundle\Security\Firewall;
 use OpenSky\Bundle\GigyaBundle\Security\Authentication\Token\GigyaToken;
 use OpenSky\Bundle\GigyaBundle\Socializer\Buzz\MessageFactory;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Http\Firewall\AbstractAuthenticationListener;
 
 class GigyaListener extends AbstractAuthenticationListener
 {
+    /**
+     * @var OpenSky\Bundle\GigyaBundle\Socializer\Buzz\MessageFactory
+     */
     private $factory;
 
+    /**
+     * @var Symfony\Component\Routing\RouterInterface
+     */
+    private $router;
+
+    /**
+     * @var string
+     */
+    private $route;
+
+    /**
+     * @param OpenSky\Bundle\GigyaBundle\Socializer\Buzz\MessageFactory $factory
+     * @param string                                                    $route
+     */
     public function setMessageFactory(MessageFactory $factory)
     {
         $this->factory = $factory;
+    }
+
+    /**
+     * @param Symfony\Component\Routing\RouterInterface $router
+     */
+    public function setRouter(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * @param string $route
+     */
+    public function setRoute($route)
+    {
+        $this->route = $route;
     }
 
     protected function attemptAuthentication(Request $request)
@@ -21,7 +55,7 @@ class GigyaListener extends AbstractAuthenticationListener
         $code = $request->query->get('code');
 
         if (null !== $code) {
-            $this->factory->setRedirectUri($request->getUriForPath($request->getPathInfo()));
+            $this->factory->setRedirectUri($this->router->generate($this->route, array(), true));
 
             return $this->authenticationManager->authenticate(new GigyaToken('', $code, $this->providerKey));
         }
